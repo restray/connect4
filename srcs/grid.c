ead64a3584471b0c8b31433f3db1cd071525c3bc
Restray <contact@restray.org>
2022-06-12 14:57:04 +0200

    patch(output): patch compilation and rollback

--- srcs/grid.c
+++ srcs/grid.c
@@ -6,19 +6,19 @@
 /*   By: tbelhomm <tbelhomm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 22:04:42 by tbelhomm          #+#    #+#             */
-/*   Updated: 2022/06/12 14:55:11 by tbelhomm         ###   ########.fr       */
+/*   Updated: 2022/06/12 14:56:33 by tbelhomm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "connect4.h"
 
-int    ft_allocate_grid(t_connect4 *setup)
+int ft_allocate_grid(t_connect4 *setup)
 {
 	setup->grid = malloc(sizeof(int *) * (setup->lines + 1));
 	if (!(setup->grid))
 	{
 		ft_printf("Mayday!! It seems we have an important issue captain!!\n");
-		return (1);
+		return (0);
 	}
 	ft_memset(setup->grid, 0, sizeof(int *) * (setup->lines + 1));
 
@@ -29,16 +29,16 @@ int    ft_allocate_grid(t_connect4 *setup)
 		if (!(setup->grid[i]))
 		{
 			ft_printf("Mayday!! It seems we have an important issue captain, we can't make a column?!\n");
-			return (1);
+			return (0);
 		}
 		ft_memset(setup->grid[i], 0, sizeof(int) * (setup->columns + 1));
 		i++;
 	}
 
-	return (0);
+	return (1);
 }
 
-void    ft_deallocate_grid(t_connect4 *setup)
+void ft_deallocate_grid(t_connect4 *setup)
 {
 	int i = 0;
 	while (i < setup->lines)
@@ -56,7 +56,7 @@ static void ft_get_color_winner(int round, int winner)
 		ft_printf("%s", ((round == 1) ? RED : YELLOW));
 }
 
-void    ft_display_grid(t_connect4 *setup, int round, int winner)
+void ft_display_grid(t_connect4 *setup, int winner)
 {
 	ft_printf("%s", CLEAR);
 
@@ -65,7 +65,7 @@ void    ft_display_grid(t_connect4 *setup, int round, int winner)
 		nb_col_size = 3;
 
 	// ///////////////////
-	ft_get_color_winner(round, winner);
+	ft_get_color_winner(setup->first_player, winner);
 	for (int y = 0; y < (setup->columns * nb_col_size) + 2; y++)
 	{
 		if (y == 0)
@@ -82,7 +82,7 @@ void    ft_display_grid(t_connect4 *setup, int round, int winner)
 		NOCOLOUR();
 		ft_putchar('\n');
 		// ///////////////////
-		ft_get_color_winner(round, winner);
+		ft_get_color_winner(setup->first_player, winner);
 		ft_printf("%s", GRID_LEFT_RIGHT);
 		NOCOLOUR();
 		for (int y = 0; y < setup->columns; y++)
@@ -94,26 +94,27 @@ void    ft_display_grid(t_connect4 *setup, int round, int winner)
 			if (setup->grid[i][y] == CELL_EMPTY)
 				ft_printf("%s", CELL_CHAR_EMPTY);
 			else if (setup->grid[i][y] == CELL_IA)
-				ft_printf("%s", ((round == 0) ? CELL_CHAR_IA : CELL_CHAR_ENNEMY));
+				ft_printf("%s", ((setup->first_player == 0) ? CELL_CHAR_IA : CELL_CHAR_ENNEMY));
 			else if (setup->grid[i][y] == CELL_PLAYER)
-				ft_printf("%s", ((round == 0) ? CELL_CHAR_ENNEMY : CELL_CHAR_IA));
+				ft_printf("%s", ((setup->first_player == 0) ? CELL_CHAR_ENNEMY : CELL_CHAR_IA));
 			else
 				ft_printf("Gone wrong... %i\n", setup->grid[i][y]);
-			for (int nb_spaces = 0; nb_spaces < nb_col_size - 2; nb_spaces++) {
+			for (int nb_spaces = 0; nb_spaces < nb_col_size - 2; nb_spaces++)
+			{
 				ft_putchar(' ');
 			}
 		}
 		// ///////////////////
 		NOCOLOUR();
-		ft_get_color_winner(round, winner);
+		ft_get_color_winner(setup->first_player, winner);
 		ft_printf("%s", GRID_LEFT_RIGHT);
 		NOCOLOUR();
 	}
 	ft_putchar('\n');
-	ft_get_color_winner(round, winner);
+	ft_get_color_winner(setup->first_player, winner);
 	// ///////////////////
 	for (int y = 0; y < (setup->columns * 3) + 2; y++)
-	{		
+	{
 		if (y == 0)
 			ft_printf("%s", GRID_BOTTOM_LEFT);
 		else if (y == (setup->columns * 3) + 1)
