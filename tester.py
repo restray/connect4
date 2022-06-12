7c2a043a71934cec2d654a366131fc178945f56a
Restray <contact@restray.org>
2022-06-12 17:56:30 +0200

    feat(tester): add output log

--- tester.py
+++ tester.py
@@ -27,7 +27,7 @@ def main(size_line: int, size_column: int):
         print(process.stdout[process.stdout.rindex("\033[1;1H\033[2J")+len("\033[1;1H\033[2J"):])
         print(test_cases[0:nb_time_prompt])
         process.stdout = process.stdout.replace("> ", "> \n")
-        with open(datetime.now().strftime('error_%H_%M_%d_%m_%Y-%f.log'), "w") as f:
+        with open(datetime.now().strftime("error_grid_{}_{}_%H_%M_%d_%m_%Y-%f.log".format(size_line, size_column)), "w") as f:
             f.write(escape_ansi(process.stdout))
             f.write("{}".format(test_cases[0:nb_time_prompt]))
             f.close()
@@ -37,18 +37,12 @@ def main(size_line: int, size_column: int):
 if __name__ == "__main__":
     print("Start simulation on 6 x 7")
     i = 0
-    while main(6, 7) and i < 500:
+    while i < 500:
+        main(6, 7)
         i += 1
-    if i == 500:
-        print("Well done, no issue for this!")
-    else:
-        print("Fail on {}".format(i))
 
     print("Start simulation on 10 x 10")
     i = 0
-    while main(10, 10) and i < 500:
+    while i < 500:
+        main(10, 10)
         i += 1
-    if i == 500:
-        print("Well done, no issue for this!")
-    else:
-        print("Fail on {}".format(i))
